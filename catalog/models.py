d98d349f28b4742033d1489fcddb9d9c983ca2c1
Hamanist <hamanist@gmail.com>
2024-02-21 22:04:05 +0300

    add models Product

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200, unique=True, verbose_name='название продукта')
+    description = models.TextField()
+    image = models.ImageField(upload_to='product/', null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=7, decimal_places=2, verbose_name='цена')
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+
+
