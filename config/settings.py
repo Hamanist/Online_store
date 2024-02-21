9831be78abaa735914c1cc52cf80f0ef603ded62
Hamanist <hamanist@gmail.com>
2024-02-21 20:04:43 +0300

    add database settings

--- config/settings.py
+++ config/settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'store',
+        'USER': 'postgres',
+        'PASSWORD': '5254',
+        'HOST': '127.0.0.1',
+        'PORT': 5432
+
     }
 }
 
