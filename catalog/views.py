f705be458644bca38d23600c863676749d89ce4c
Hamanist <hamanist@gmail.com>
2024-03-06 22:55:27 +0300

    add views goods and context title

--- catalog/views.py
+++ catalog/views.py
@@ -1,16 +1,35 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'catalog/home.html')
+    object_list = Product.objects.all()
+    context = {
+        'objects': object_list,
+        'title': 'Главная',
+    }
+    return render(request, 'catalog/home.html', context=context)
 
 
 def contacts(request):
+    context = {
+        'title': 'Контакты',
+    }
     if request.method == 'POST':
         name = request.POST.get('name')
         phone = request.POST.get('phone')
         message = request.POST.get('message')
         print(f'{name} {phone}\n{message}')
 
-    return render(request, 'catalog/contacts.html')
+    return render(request, 'catalog/contacts.html', context)
+
+
+def goods(request, pk):
+    object_list_goods = Product.objects.get(pk=pk)
+    context = {
+        'object': object_list_goods,
+        'title': 'Описание',
+    }
+    return render(request, 'catalog/goods.html', context)
