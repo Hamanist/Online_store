c1dad15a085bc29974796ce9d2019d4815d8f1d5
Hamanist <hamanist@gmail.com>
2024-03-06 22:53:19 +0300

    add app_name and new path

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
-from catalog.views import home, contacts
+from catalog.views import home, contacts, goods
+from catalog.apps import CatalogConfig
+
+app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home),
-    path('contacts/', contacts)
+    path('', home, name='home'),
+    path('contacts/', contacts, name='contacts'),
+    path('goods/<int:pk>', goods, name='goods'),
 ]
