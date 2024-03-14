95e1069c13ca7da337832ef36f884eac00d55407
Hamanist <hamanist@gmail.com>
2024-03-14 21:32:11 +0300

    add CatalogListView

--- catalog/views.py
+++ catalog/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
 from catalog.models import Product
 
 
-# Create your views here.
-def home(request):
-    object_list = Product.objects.all()
-    context = {
-        'objects': object_list,
-        'title': 'Главная',
-    }
-    return render(request, 'catalog/home.html', context=context)
+class CatalogListView(ListView):
+    model = Product
+
+# def home(request):
+#     object_list = Product.objects.all()
+#     context = {
+#         'objects': object_list,
+#         'title': 'Главная',
+#     }
+#     return render(request, 'catalog/product_list.html', context=context)
 
 
 def contacts(request):
@@ -26,10 +29,15 @@ def contacts(request):
     return render(request, 'catalog/contacts.html', context)
 
 
-def goods(request, pk):
-    object_list_goods = Product.objects.get(pk=pk)
-    context = {
-        'object': object_list_goods,
-        'title': 'Описание',
-    }
-    return render(request, 'catalog/goods.html', context)
+class DetailList:
+    pass
+
+
+# class ProductDetailList(DetailList):
+# def goods(request, pk):
+#     object_list_goods = Product.objects.get(pk=pk)
+#     context = {
+#         'object': object_list_goods,
+#         'title': 'Описание',
+#     }
+#     return render(request, 'catalog/goods.html', context)
