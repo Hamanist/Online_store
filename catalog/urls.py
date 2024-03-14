40feb8eae805bf1c8c014d12e6df1799ff15d4ce
Hamanist <hamanist@gmail.com>
2024-03-14 21:33:02 +0300

    changed path

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from catalog.views import home, contacts, goods
+from catalog.views import contacts, goods, CatalogListView
 from catalog.apps import CatalogConfig
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', CatalogListView.as_view(), name='home'),
     path('contacts/', contacts, name='contacts'),
     path('goods/<int:pk>', goods, name='goods'),
 ]
