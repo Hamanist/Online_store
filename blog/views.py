d1d420d43da6e3ce06b98d9aaeed33343d066e04
Hamanist <hamanist@gmail.com>
2024-03-16 17:22:16 +0300

    add BlogCreateView

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
 
-# Create your views here.
+from blog.models import Blog
+
+
+class BlogListView(ListView):
+    model = Blog
+    extra_context = {'title': 'Блог'}
+
+
+class BlogDetailView(DetailView):
+    model = Blog
+    extra_context = {'title': 'Описание Блога'}
+
+
+class BlogCreateView(CreateView):
+    model = Blog
+    fields = ('title', 'content', 'preview',)
+    success_url = reverse_lazy('blog:list')
+
+
+
+
