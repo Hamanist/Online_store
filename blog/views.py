ddb1acac24ed78943afd2190122b6dedc4d013ad
Hamanist <hamanist@gmail.com>
2024-03-17 12:56:44 +0300

    add form_valid for slug

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
+
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from pytils.translit import slugify
 
 from blog.models import Blog
 
@@ -30,6 +32,13 @@ class BlogCreateView(CreateView):
     model = Blog
     fields = ('title', 'content', 'preview',)
     success_url = reverse_lazy('blog:list')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_blog = form.save()
+            new_blog.slug = slugify(new_blog.title)
+            new_blog.save()
+        return super().form_valid(form)
 
 
 class BlogUpdateView(UpdateView):
