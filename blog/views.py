43994d4a0d5612fcb6e2bb1f792f0ac551b6b29d
Hamanist <hamanist@gmail.com>
2024-03-17 12:10:23 +0300

    add BlogUpdateView and BlogDeleteView

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from blog.models import Blog
 
@@ -14,6 +14,8 @@ class BlogDetailView(DetailView):
     model = Blog
     extra_context = {'title': 'Описание Блога'}
 
+    # def get_object(self, queryset=None):
+
 
 class BlogCreateView(CreateView):
     model = Blog
@@ -21,5 +23,12 @@ class BlogCreateView(CreateView):
     success_url = reverse_lazy('blog:list')
 
 
+class BlogUpdateView(UpdateView):
+    model = Blog
+    fields = ('title', 'content', 'preview',)
+    success_url = reverse_lazy('blog:list')
 
 
+class BlogDeleteView(DeleteView):
+    model = Blog
+    success_url = reverse_lazy('blog:list')
